99f67596a79d488be24fae1d4f51c266537f70b0
nFinityV2 <nFinitySoh@hotmail.com>
2024-07-10 13:25:57 +1000

    Added missing states for inputs and js

---
+++ index.js
@@ -0,0 +1,4 @@
+const bill = document.getElementById("bill");
+const people = document.getElementById("people");
+const tip = document.getElementById("custom-tip");
+const reset = document.getElementById("reset");
